2485bd48dd65b72cb1f83331e4cb7fad608d44cd
Valdeci Gomes <valdeci@coderade.in>
2016-01-12 21:34:29 -0200

    ready to deploy

---
+++ server.js
@@ -0,0 +1,71 @@
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: function (str, path){
+            return stylus(str).set('filename', path)
+        }
+    }
+));
+
+app.use(express.static(__dirname + '/public'));
+
+if(env === 'development'){
+    mongoose.connect('mongodb://localhost/meancourses');
+}
+else{
+    mongoose.connect('mongodb://admin:meancourses@ds043605.mongolab.com:43605/mean-courses');
+}
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+
+db.once('open', function callback() {
+    console.log('meancourses db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String}),
+    Message = mongoose.model = mongoose.model('Message', messageSchema),
+    mongoMessage;
+
+Message.findOne().exec(function (err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
+
+
+
+app.get('/partials/:partialPath', function (req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function (req,res) {
+    res.render('index', {
+        mongoMessage : mongoMessage
+    })
+});
+
+
+
+
+var port = process.env.PORT || 3030;
+app.listen(port);
+
+console.log('Listening on the port ' + port + '...')
